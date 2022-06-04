4539d6fcaf73b2a2e8a27b6ef7e8aee22cd25084
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-06-04 02:30:44 -0600

    update large button, add select flag

--- lua/scripts/ui/components/largeButton.lua
+++ lua/scripts/ui/components/largeButton.lua
@@ -1,7 +1,8 @@
 ---@param justification '"left_justify"' | '"center_justify"' | '"right_justify"'
 ---@return invaderWidget
-return function(justification)
-    return {
+return function(justification, select)
+    ---@type invaderWidget
+    local wid = {
         bounds = "0 0 24 389",
         background_bitmap = [[insurrection\ui\bitmaps\large_button.bitmap]],
         text_font = [[ui\large_ui.font]],
@@ -10,4 +11,8 @@ return function(justification)
         horiz_offset = 10,
         vert_offset = 5
     }
+    if select then
+        wid.background_bitmap = [[insurrection\ui\bitmaps\large_button_select.bitmap]]
+    end
+    return wid
 end
