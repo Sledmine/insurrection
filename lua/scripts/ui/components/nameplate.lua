253345ad073fb42006cf4061b2ec3acb6b765e91
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-08-14 00:36:33 -0600

    update nameplate component

--- lua/scripts/ui/components/nameplate.lua
+++ lua/scripts/ui/components/nameplate.lua
@@ -1,4 +1,5 @@
 local ustr = require "lua.scripts.modules.ustr"
+local widget = require "lua.scripts.widget"
 ---Nameplate widget
 ---@return invaderWidget
 return function(name, text)
@@ -9,42 +10,27 @@ return function(name, text)
                              [[.unicode_string_list]]
         ustr(stringsTagPath, {text})
     end
-    ---@type invaderWidget
-    local widget = {
+    local overlayPath = "insurrection/ui/menus/lobby/buttons/" .. name ..
+                            "_overlay.ui_widget_definition"
+    widget.create(overlayPath, {
         widget_type = "text_box",
         bounds = "-26 -192 26 192",
-        --flags = {pass_unhandled_events_to_focused_child = true},
-        background_bitmap = [[insurrection/ui/bitmaps/default_nameplate.bitmap]],
-        event_handlers = {
-            {
-                flags = {
-                    open_widget = true,
-                    run_function = true,
-                    go_back_to_previous_widget = back or false
-                },
-                event_type = "a_button",
-                widget_tag = openTag or ".ui_widget_definition",
-                script = script or ""
-            },
-            {
-                flags = {run_function = true},
-                event_type = "left_mouse",
-                ["function"] = "mouse_emit_accept_event",
-                widget_tag = openTag or ".ui_widget_definition"
-            }
-        },
+        background_bitmap = [[insurrection/ui/shared/bitmaps/nameplate_overlay.bitmap]],
+        -- game_data_inputs = {{["function"] = "get_active_plyr_profile_name"}},
         text_label_unicode_strings_list = stringsTagPath,
         string_list_index = 0,
         text_font = [[ui/large_ui.font]],
-        text_color = "1 1 1 1",
+        text_color = "1 0.8 0.8 0.8",
         justification = "left_justify",
         horiz_offset = 225,
-        vert_offset = 31,
-        child_widgets = {
-            {
-                widget_tag = [[insurrection/ui/shared/nameplate_overlay.ui_widget_definition]]
-            }
-        }
+        vert_offset = 31
+    })
+    ---@type invaderWidget
+    local widget = {
+        widget_type = "container",
+        bounds = "-26 -192 26 192",
+        background_bitmap = [[insurrection/ui/bitmaps/default_nameplate.bitmap]],
+        child_widgets = {{widget_tag = overlayPath}}
     }
     return widget
 end
