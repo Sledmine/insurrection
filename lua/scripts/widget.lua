d7effba384bc00727597e1542782778cd20c74c5
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-08-14 17:52:24 -0600

    fix class name in widget module

--- lua/scripts/widget.lua
+++ lua/scripts/widget.lua
@@ -57,7 +57,7 @@ local widget = {}
 ---@field flags invaderWidgetConditionalWidgetFlags
 ---@field custom_controller_index number
 
----@class invaderWidgetGameDataInputs
+---@class invaderWidgetGameDataInput
 ---@field function string
 
 ---@class invaderWidget
@@ -67,7 +67,7 @@ local widget = {}
 ---@field milliseconds_to_auto_close number
 ---@field milliseconds_to_auto_close_fade_time number
 ---@field background_bitmap string
----@field game_data_inputs invaderWidgetGameDataInputs[]
+---@field game_data_inputs invaderWidgetGameDataInput[]
 ---@field event_handlers invaderWidgetEventHandler[]
 ---@field extended_description_widget string
 ---@field justification '"left_justify"' | '"center_justify"' | '"right_justify"'
