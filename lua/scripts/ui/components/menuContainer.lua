31fc6fdbf5d9292484dd8772418ee7acd729c753
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-09-11 20:26:15 -0600

    some component changes

--- lua/scripts/ui/components/menuContainer.lua
+++ lua/scripts/ui/components/menuContainer.lua
@@ -1,9 +1,10 @@
 ---Menu container component, this is the first component that holds a screen
 ---@return invaderWidget
-return function()
+return function(childWidgets)
     return {
         bounds = "0 0 480 856",
         background_bitmap = [[insurrection\ui\bitmaps\background_transparent.bitmap]],
-        flags = {pass_unhandled_events_to_focused_child = true}
+        flags = {pass_unhandled_events_to_focused_child = true},
+        childWidgets = childWidgets or {}
     }
 end
