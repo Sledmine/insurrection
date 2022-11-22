0539eb451179e45de0a1224d78f70e398ed747fc
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-11-22 00:21:16 -0600

    improve checkbox dynamic component

--- lua/insurrection/components/checkbox.lua
+++ lua/insurrection/components/checkbox.lua
@@ -2,7 +2,10 @@ local core = require "insurrection.core"
 local components = require "insurrection.components"
 
 ---@class uiComponentCheckboxClass : uiComponentClass
-local checkbox = setmetatable({}, {__index = components})
+local checkbox = setmetatable({
+    ---@type number
+    checkboxTagId = nil
+}, {__index = components})
 
 ---@class uiComponentCheckboxEvents : uiComponentEvents
 ---@field onToggle fun(value: boolean):boolean | nil
@@ -13,23 +16,40 @@ local checkbox = setmetatable({}, {__index = components})
 ---@return uiComponentCheckbox
 function checkbox.new(tagId)
     local instance = setmetatable(components.new(tagId), {__index = checkbox}) --[[@as uiComponentCheckbox]]
+    assert(instance.tag.path:find("checkbox", 1, true),
+           "Tag " .. instance.tag.path .. " is not a checkbox")
+    instance.checkboxTagId = instance:findChildWidgetTag("checkbox").id
     return instance
+end
+
+---@param self uiComponentCheckbox
+---@return boolean
+function checkbox.getValue(self)
+    local widgetValues = core.getWidgetValues(self.checkboxTagId)
+    assert(widgetValues, "Checkbox " .. self.tag.path .. " does not exist in DOM tree")
+    return widgetValues.background_bitmap_index == 1
+end
+
+function checkbox.setValue(self, value)
+    local value = value and 1 or 0
+    core.setWidgetValues(self.checkboxTagId, {background_bitmap_index = value})
+end
+
+function checkbox.toggle(self)
+    local value = self:getValue()
+    self:setValue(not value)
 end
 
 ---@param self uiComponentCheckbox
 function checkbox.onToggle(self, callback)
     self.events.onClick = function()
-        local checkboxTagId = self:findChildWidgetTag("checkbox").id
-        local bitmapIndex = core.getWidgetValues(checkboxTagId).background_bitmap_index
-        if bitmapIndex == 0 then
-            core.setWidgetValues(checkboxTagId, {background_bitmap_index = 1})
-        else
-            core.setWidgetValues(checkboxTagId, {background_bitmap_index = 0})
-        end
-        if bitmapIndex == 0 then
-            callback(true)
-        else
+        local value = self:getValue()
+        if value then
+            self:toggle()
             callback(false)
+        else
+            self:toggle()
+            callback(true)
         end
     end
 end
