cfcf5411a62b95207353ff4a58e64c9bce8a959b
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-01-30 01:36:47 -0600

    automate better version building

--- lua/insurrection/version.lua
+++ lua/insurrection/version.lua
@@ -1,1 +1,7 @@
-return "2.4.0"
+local metadata = ""
+local releaseVersion = "2.4.0"
+local version = releaseVersion
+if DebugMode then
+    return releaseVersion .. "-dev+" .. metadata
+end
+return version
