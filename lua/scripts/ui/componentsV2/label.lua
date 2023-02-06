5e5a89f0bc9f6cbf54370c240f6c5e433153b5b6
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-02-06 00:42:12 -0600

    improve label component

--- lua/scripts/ui/componentsV2/label.lua
+++ lua/scripts/ui/componentsV2/label.lua
@@ -5,21 +5,31 @@ local ustr = require "lua.scripts.modules.ustr"
 
 ---Label component
 ---@param name string Name of the component
----@param text string Text to display
----@param props? {childs: invaderWidgetChildWidget[]} Properties of the component
+---@param text string | string[] Text to display
+---@param props? {childs: invaderWidgetChildWidget[], variant?: '"title"' | '"subtitle"' | '"text"'} Properties of the component
 ---@return string
 return function(name, text, props)
     local props = props or {}
     local widgetPath = widget.path .. name .. "_label.ui_widget_definition"
     local stringsPath = widget.path .. "strings/" .. name .. "_label.unicode_string_list"
-    ustr(stringsPath, {text})
+    if type(text) == "string" then
+        ustr(stringsPath, {text})
+    else
+        ustr(stringsPath, text)
+    end
+    local textFont = constants.fonts.text
+    if props.variant == "title" then
+        textFont = constants.fonts.title
+    elseif props.variant == "subtitle" then
+        textFont = constants.fonts.subtitle
+    end
     ---@type invaderWidget
     local wid = {
         widget_type = "text_box",
         bounds = "0, 0, 24, 184",
         flags = {pass_unhandled_events_to_focused_child = true},
-        text_font = constants.fonts.text,
-        text_color = constants.color.selected,
+        text_font = textFont,
+        text_color = constants.color.text,
         text_label_unicode_strings_list = stringsPath,
         string_list_index = 0,
         horiz_offset = 10,
