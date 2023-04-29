b4613373a19f0863cd1d9fbe8383ffae6bad0a00
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-04-29 16:47:42 -0600

    update version

--- lua/insurrection/version.lua
+++ lua/insurrection/version.lua
@@ -1,5 +1,5 @@
-local releaseVersion = "2.4.2"
-local metadata = "29b15b1.20230428"
+local releaseVersion = "2.4.3"
+local metadata = "3a7cb3d.20230429"
 local version = releaseVersion
 if DebugMode then
     return releaseVersion .. "-dev+" .. metadata
