3c2e23f88c9d94d98af7d9c4f3c448d483d65ba8
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-05-13 14:01:35 -0600

    add components v3

---
+++ lua/scripts/ui/componentsV3/options.lua
@@ -0,0 +1,139 @@
+local widget = require "lua.scripts.widget"
+local constants = require "lua.scripts.ui.components.constants"
+
+---@class optionsProps
+---@field name string
+---@field alignment '"vertical"' | '"horizontal"'
+---@field [1]? invaderWidgetChildWidget[]
+---@field childs? invaderWidgetChildWidget[]
+---@field description? string Tag path for description to display
+---@field func? string
+---@field branch? boolean
+---@field conditionalWidgets? invaderWidgetConditionalWidget[]
+---@field dataInput? string
+---@field eventsToChildren? boolean
+---@field useItems? boolean
+---@field campaignMaps? boolean
+---@field multiplayerMaps? string
+---@field gametypes string
+---@field colorPicker? boolean
+---@field width number
+---@field height number
+
+---Options list component, scroll trough elements using dpad, etc
+---@param props optionsProps
+---@return string
+return function(props)
+    local name = props.name
+    local alignment = props.alignment or "vertical"
+    local childWidgets = props[1] or props.childs
+    local description = props.description
+
+    local widgetPath = widget.path .. name .. "_options.ui_widget_definition"
+    local bounds
+    if props.width or props.height then
+        bounds = widget.bounds(0, 0, props.height or 0, props.width or 0)
+    end
+    local horizontal = alignment == "horizontal"
+    ---@type invaderWidget
+    local wid = {
+        widget_type = "column_list",
+        -- For debug purposes
+        --background_bitmap = [[insurrection/ui/bitmaps/solid_green.bitmap]],
+        bounds = bounds or constants.getScreenBounds(),
+        flags = {
+            pass_unhandled_events_to_focused_child = true,
+            dpad_up_down_tabs_thru_children = not horizontal,
+            dpad_left_right_tabs_thru_children = horizontal,
+            pass_handled_events_to_all_children = props.eventsToChildren or false
+        },
+        child_widgets = childWidgets or {},
+        event_handlers = {},
+        extended_description_widget = description or ".ui_widget_definition",
+        conditional_widgets = props.conditionalWidgets
+    }
+    if props.useItems then
+        wid.flags.dpad_up_down_tabs_thru_children = false
+        wid.flags.dpad_left_right_tabs_thru_children = false
+        wid.flags.dpad_up_down_tabs_thru_list_items = not horizontal
+        wid.flags.dpad_left_right_tabs_thru_list_items = horizontal
+    end
+    if props.dataInput then
+        wid.game_data_inputs = {{["function"] = props.dataInput}}
+    end
+    if props.func or props.branch then
+        wid.event_handlers = {
+            {
+                event_type = "created",
+                flags = {run_function = true, try_to_branch_on_failure = props.branch ~= nil},
+                ["function"] = props.func
+            }
+        }
+    end
+    if props.campaignMaps then
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "deleted",
+            flags = {run_function = true},
+            ["function"] = "dispose_sp_level_list"
+        }
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "custom_activation",
+            flags = {run_function = true, open_widget = true},
+            -- ["function"] = "init_sp_level_list"
+            ["function"] = "solo_level_set_map",
+            widget_tag = [[insurrection/ui/menus/classic_campaign_difficulty_select/classic_campaign_difficulty_select_menu.ui_widget_definition]]
+        }
+    elseif props.multiplayerMaps then
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "created",
+            flags = {run_function = true},
+            ["function"] = "mp_level_list_initialize"
+        }
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "deleted",
+            flags = {run_function = true},
+            ["function"] = "mp_level_list_dispose"
+        }
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "custom_activation",
+            flags = {run_function = true, open_widget = true},
+            ["function"] = "mp_level_select",
+            widget_tag = props.multiplayerMaps
+        }
+    elseif props.gametypes then
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "created",
+            flags = {run_function = true},
+            ["function"] = "mp_profiles_list_initialize"
+        }
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "deleted",
+            flags = {run_function = true},
+            ["function"] = "mp_profiles_list_dispose"
+        }
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "custom_activation",
+            flags = {run_function = true, open_widget = true},
+            ["function"] = "mp_profile_set_for_game",
+            widget_tag = props.gametypes
+        }
+    elseif props.colorPicker then
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "created",
+            flags = {run_function = true},
+            ["function"] = "color_picker_menu_initialize"
+        }
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "deleted",
+            flags = {run_function = true},
+            ["function"] = "color_picker_menu_dispose"
+        }
+        wid.event_handlers[#wid.event_handlers + 1] = {
+            event_type = "custom_activation",
+            flags = {run_function = true},
+            ["function"] = "color_picker_select_color"
+        }
+    end
+    widget.createV2(widgetPath, wid)
+    return widgetPath
+end
---
+++ lua/scripts/ui/componentsV3/wrapper.lua
@@ -0,0 +1,24 @@
+local constants = require "lua.scripts.ui.components.constants"
+local widget = require "lua.scripts.widget"
+
+---Wrapper component serves the purpose of wrapping widgets as a container given size
+---@param props {name: string, width: number, height: number, [1]: invaderWidgetChildWidget[]}
+---@return string
+return function(props)
+    local name = props.name
+    local childWidgets = props[1]
+    local width = props.width
+    local height = props.height
+    local path = widget.path .. name .. "_wrapper.ui_widget_definition"
+
+    ---@type invaderWidget
+    local wid = {
+        bounds = widget.bounds(0, 0, height, width),
+        background_bitmap = "insurrection/ui/bitmaps/solid_green.bitmap",
+        flags = {pass_unhandled_events_to_focused_child = true},
+        child_widgets = childWidgets or {}
+    }
+
+    widget.createV2(path, wid)
+    return path
+end
