693755b796d372598a190170bde63de49657e46c
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-09-10 22:54:42 -0600

    update requestscurl module

--- lua/lua_modules/requestscurl.lua
+++ lua/lua_modules/requestscurl.lua
@@ -1,6 +1,42 @@
 local json = require "json"
 local requests = {}
 
+requests.codes = {
+    ok = 200,
+    created = 201,
+    accepted = 202,
+    no_content = 204,
+    moved_permanently = 301,
+    found = 302,
+    see_other = 303,
+    not_modified = 304,
+    temporary_redirect = 307,
+    bad_request = 400,
+    unauthorized = 401,
+    forbidden = 403,
+    not_found = 404,
+    method_not_allowed = 405,
+    not_acceptable = 406,
+    request_timeout = 408,
+    conflict = 409,
+    gone = 410,
+    length_required = 411,
+    precondition_failed = 412,
+    payload_too_large = 413,
+    uri_too_long = 414,
+    unsupported_media_type = 415,
+    range_not_satisfiable = 416,
+    expectation_failed = 417,
+    im_a_teapot = 418,
+    unprocessable_entity = 422,
+    too_many_requests = 429,
+    internal_server_error = 500,
+    not_implemented = 501,
+    bad_gateway = 502,
+    service_unavailable = 503,
+    gateway_timeout = 504
+}
+
 requests.headers = {}
 requests.timeout = 300
 
