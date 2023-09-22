9591dddb097936f4a87fa9f34d5d718ee3540ec7
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-09-22 02:28:50 -0600

    refactor redux store

--- lua/insurrection/redux/actions.lua
+++ lua/insurrection/redux/actions.lua
@@ -4,14 +4,9 @@ local actions = {}
 actions.types = {
     SET_IS_LOADING = "SET_IS_LOADING",
     SET_LOBBY = "SET_LOBBY",
-    UPDATE_LOBBY = "UPDATE_LOBBY",
-    SET_LOBBY_DEFINITION = "SET_LOBBY_DEFINITION",
-    SET_SELECTED = "SET_SELECTED",
-    SET_SELECTED_ITEM = "SET_SELECTED_ITEM",
-    SCROLL_LIST = "SCROLL_LIST",
-    SET_LIST = "SET_LIST",
     SET_AVAILABLE_RESOURCES = "SET_AVAILABLE_RESOURCES",
-    CLEANUP = "CLEANUP",
+    RESET = "RESET",
+    SET_PLAYER = "SET_PLAYER"
 }
 
 function actions.setIsLoading(loading)
@@ -22,48 +17,16 @@ function actions.setLobby(key, lobby)
     return {type = actions.types.SET_LOBBY, payload = {key = key, lobby = lobby}}
 end
 
-function actions.updateLobby(key, lobby, filter)
-    return {
-        type = actions.types.UPDATE_LOBBY,
-        payload = {key = key, lobby = lobby, filter = filter}
-    }
-end
-
----@param definition '"template"' | '"map"' | '"gametype"'
----@return table
-function actions.setLobbyDefinition(definition)
-    return {type = actions.types.SET_LOBBY_DEFINITION, payload = definition}
-end
-
-function actions.setSelected(element)
-    return {type = actions.types.SET_SELECTED, payload = element}
-end
-
-function actions.setSelectedItem(element)
-    return {type = actions.types.SET_SELECTED_ITEM, payload = element}
-end
-
-function actions.scroll(scrollNext)
-    return {
-        type = actions.types.SCROLL_LIST,
-        payload = {scrollNext = scrollNext}
-    }
-end
-
-function actions.setList(list, chunkSize)
-    return {type = actions.types.SET_LIST, payload = {list = list, chunkSize = chunkSize}}
-end
-
 function actions.reset()
     return {type = redux.actionTypes.INIT}
 end
 
-function actions.clean()
-    return {type = actions.types.CLEANUP}
-end
-
 function actions.setAvailableResources(available)
     return {type = actions.types.SET_AVAILABLE_RESOURCES, payload = available}
+end
+
+function actions.setPlayer(player)
+    return {type = actions.types.SET_PLAYER, payload = player}
 end
 
 return actions
