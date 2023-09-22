d15624b95ccfa43307fe2c312bfea17e21a4bac3
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-09-22 02:28:50 -0600

    refactor redux store

--- lua/insurrection/redux/reducers/interfaceReducer.lua
+++ lua/insurrection/redux/reducers/interfaceReducer.lua
@@ -4,32 +4,28 @@ local glue = require "glue"
 local chunks = glue.chunks
 
 ---@class interfaceState
+---@field isLoading boolean
+---@field player insurrectionPlayer?
+---@field available availableParameters?
+---@field lobby insurrectionLobby?
+
+---@type interfaceState
 local defaultState = {
     isLoading = false,
-    definition = "template",
     lobbyKey = nil,
-    available = {maps = {}, gametypes = {}, templates = {}, customization = {}},
-    lobby = {owner = "", map = "", gametype = "", template = "", players = {}},
-    ---@type table | string | number
-    selected = {template = nil, map = nil, gametype = nil},
-    displayed = {},
-    filtered = {},
-    list = {},
-    currentChunk = 1,
-    chunkSize = 4
+    available = nil,
+    lobby = nil,
+    player = nil
 }
 
 ---Game interface reducer
 ---@param state interfaceState
 ---@param action reduxAction
+---@return interfaceState
 local function interfaceReducer(state, action)
     dprint(action.type, "info")
     if action.type == redux.actionTypes.INIT then
         return table.copy(defaultState)
-    elseif action.type == actions.types.CLEANUP then
-        local clean = table.copy(defaultState)
-        clean.available = state.available
-        return clean
     elseif action.type == actions.types.SET_IS_LOADING then
         state.isLoading = action.payload
         return state
@@ -38,93 +34,11 @@ local function interfaceReducer(state, action)
         dprint(state)
         return state
     elseif action.type == actions.types.SET_LOBBY then
-        state.lobbyKey = action.payload.key
         state.lobby = action.payload.lobby
-        local available = state.available
-        state.chunkSize = 4
-        state.currentChunk = 1
-        state.list = available[state.definition .. "s"]
-        state.displayed = chunks(available.templates, state.chunkSize)[1]
-        ---@diagnostic disable-next-line
-        state.selected = table.copy(defaultState.selected)
-        state.selected.template = state.lobby.template
-        state.selected.map = state.lobby.map
-        state.selected.gametype = state.lobby.gametype
-        return state
-    elseif action.type == actions.types.UPDATE_LOBBY then
-        if action.payload.key then
-            state.lobbyKey = action.payload.key
-        end
-        if action.payload.lobby then
-            local session = api.session
-            if session and not (session.player.publicId == action.payload.lobby.owner) then
-                state.selected.template = action.payload.lobby.template
-                state.selected.map = action.payload.lobby.map
-                state.selected.gametype = action.payload.lobby.gametype
-            end
-            state.lobby = action.payload.lobby
-        end
-        if action.payload.filter then
-            state.filtered = table.filter(state.available[state.definition .. "s"],
-                                          function(mapName)
-                return mapName:lower():includes(action.payload.filter:lower())
-            end)
-            state.currentChunk = 1
-            if state.filtered then
-                state.displayed = chunks(state.filtered, 4)[state.currentChunk] or {}
-                table.sort(state.displayed, function(a, b)
-                    if a and b then
-                        return a < b
-                    end
-                    return false
-                end)
-            end
-        end
-        return state
-    elseif action.type == actions.types.SET_LOBBY_DEFINITION then
-        state.definition = action.payload
-        state.list = state.available[state.definition .. "s"]
-        state.currentChunk = 1
-        state.chunkSize = 4
-        state.displayed = chunks(state.list, state.chunkSize)[state.currentChunk]
-        return state
-    elseif action.type == actions.types.SET_SELECTED_ITEM then
-        state.selected = action.payload
-        return state
-    elseif action.type == actions.types.SET_SELECTED then
-        state.selected[state.definition] = action.payload
-        return state
-    elseif action.type == actions.types.SET_LIST then
-        dprint(state)
-        state.chunkSize = action.payload.chunkSize or defaultState.chunkSize
-        state.currentChunk = 1
-        state.list = action.payload.list
-        state.displayed = chunks(action.payload.list, state.chunkSize)[state.currentChunk] or {}
-        return state
-    elseif action.type == actions.types.SCROLL_LIST then
-        local list = state.list
-        local chunkCount = #chunks(list, state.chunkSize)
-        -- Scroll forward
-        if not action.payload.scrollNext then
-            if (state.currentChunk < chunkCount) then
-                state.currentChunk = state.currentChunk + 1
-            end
-        else
-            if (state.currentChunk > 1) then
-                state.currentChunk = state.currentChunk - 1
-            end
-        end
-        if (list and #list > 0) then
-            local displayed = chunks(list, state.chunkSize)[state.currentChunk]
-            if displayed then
-                state.displayed = displayed
-            end
-        end
         return state
     else
         error("Undefined redux action type!")
     end
-    return state
 end
 
 return interfaceReducer
