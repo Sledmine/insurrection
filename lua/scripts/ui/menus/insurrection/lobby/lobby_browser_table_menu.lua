764de2854acd41f20cce2d607d1857c39f605dcf
markmcfuzz <magecape10@gmail.com>
2024-07-01 14:58:20 -0600

    updated lobby browser menu

--- lua/scripts/ui/menus/insurrection/lobby/lobby_browser_table_menu.lua
+++ lua/scripts/ui/menus/insurrection/lobby/lobby_browser_table_menu.lua
@@ -15,7 +15,8 @@ local buttonTable = require "lua.scripts.ui.componentsV3.buttonTable"
 
 widget.init [[insurrection/ui/menus/lobby_browser_table/]]
 
-local headersLayout = widget.align("horizontal", 104, 39, 118, 4)
+local headersLayout = widget.align("horizontal", 104, 37, 118, 0)
+local tableRowLayout = widget.layout{alignment = "vertical", size = 19, x = 33, y = 146, margin = 2}
 
 return container {
     name = "lobby_browser_table_menu",
@@ -24,7 +25,7 @@ return container {
         {
             header {
                 name = "lobby_browser_table",
-                title = strmem(256, "Insurrection - Ranked - Lobby Browser"),
+                title = strmem(256, "$ - $ - LOBBY BROWSER"),
                 subtitle = "sub_string_test"
             },
             pos.header.x,
@@ -32,21 +33,66 @@ return container {
         },
         {
             options {
-                name = "lobby_browser_table_list",
+                name = "lobby_browser_table_header_list",
                 alignment = "horizontal",
                 childs = {
                     {
                         buttonTable {
-                            name = "test_button",
+                            name = "owner_header",
+                            text = "OWNER"
                         },
                         headersLayout()
                     },
                     {
                         buttonTable {
-                            name = "test_button_2",
+                            name = "map_header",
+                            text = "MAP"
+                        },
+                        headersLayout()
+                    },
+                    {
+                        buttonTable {
+                            name = "gametype_header",
+                            text = "GAMETYPE"
+                        },
+                        headersLayout()
+                    },
+                    {
+                        buttonTable {
+                            name = "players_header",
+                            text = "PLAYERS"
+                        },
+                        headersLayout()
+                    },
+                    {
+                        buttonTable {
+                            name = "ping_header",
+                            text = "PING"
                         },
                         headersLayout()
                     }
+                }
+            }
+        },
+        {
+            options {
+                name = "lobby_browser_table_row_list",
+                alignment = "vertical",
+                childs = {
+                    {
+                        buttonTable {
+                            name = "list_browser_row_button",
+                            variant = "row"
+                        },
+                        tableRowLayout()
+                    },
+                    {
+                        buttonTable {
+                            name = "list_browser_row_button2",
+                            variant = "row"
+                        },
+                        tableRowLayout()
+                    },
                 }
             }
         }
