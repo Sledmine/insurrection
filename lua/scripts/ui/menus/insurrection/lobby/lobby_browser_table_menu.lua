064b9bd4c6d716e6db92b13245a9d063b860204d
markmcfuzz <magecape10@gmail.com>
2024-07-01 15:25:47 -0600

    add preview, scroll to lobby browser

--- lua/scripts/ui/menus/insurrection/lobby/lobby_browser_table_menu.lua
+++ lua/scripts/ui/menus/insurrection/lobby/lobby_browser_table_menu.lua
@@ -2,6 +2,7 @@ local widget = require "lua.scripts.widget"
 local constants = require "lua.scripts.ui.components.constants"
 local wrapper = require "lua.scripts.ui.componentsV3.wrapper"
 local preview = require "lua.scripts.ui.componentsV3.preview"
+local bar     = require "lua.scripts.ui.componentsV3.bar"
 local pos = constants.position
 local label = require "lua.scripts.ui.componentsV3.label"
 local strmem = widget.strmem
@@ -16,7 +17,19 @@ local buttonTable = require "lua.scripts.ui.componentsV3.buttonTable"
 widget.init [[insurrection/ui/menus/lobby_browser_table/]]
 
 local headersLayout = widget.align("horizontal", 104, 37, 118, 0)
-local tableRowLayout = widget.layout{alignment = "vertical", size = 19, x = 33, y = 146, margin = 2}
+local tableRowLayout = widget.layout{alignment = "vertical", size = 19, x = 33, y = 146, margin = 1}
+
+local rows = {}
+for i = 1, 8 do
+    table.insert(rows, {
+        buttonTable {
+            name = "list_browser_row_button_" .. i,
+            variant = "row"
+        },
+        tableRowLayout()
+    })
+end
+
 
 return container {
     name = "lobby_browser_table_menu",
@@ -30,6 +43,24 @@ return container {
             },
             pos.header.x,
             pos.header.y
+        },
+        {
+            bar {
+                name = "lobby_browser_table_scroll",
+                orientation = "vertical",
+                type = "scroll",
+                size = 185
+            },
+            588,
+            118
+        },
+        {
+            preview {
+                name = "lobby_browser_table_preview",
+                bitmap = [[insurrection/ui/bitmaps/unknown_map_preview.bitmap]]
+            },
+            621,
+            118
         },
         {
             options {
@@ -78,22 +109,7 @@ return container {
             options {
                 name = "lobby_browser_table_row_list",
                 alignment = "vertical",
-                childs = {
-                    {
-                        buttonTable {
-                            name = "list_browser_row_button",
-                            variant = "row"
-                        },
-                        tableRowLayout()
-                    },
-                    {
-                        buttonTable {
-                            name = "list_browser_row_button2",
-                            variant = "row"
-                        },
-                        tableRowLayout()
-                    },
-                }
+                childs = rows
             }
         }
     }
