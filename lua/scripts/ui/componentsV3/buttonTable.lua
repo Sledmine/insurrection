0d0a3d104189b2e63d6db76cb28d7d99f45188e7
markmcfuzz <magecape10@gmail.com>
2024-07-01 14:57:37 -0600

    updated buttonTable component

--- lua/scripts/ui/componentsV3/buttonTable.lua
+++ lua/scripts/ui/componentsV3/buttonTable.lua
@@ -20,14 +20,31 @@ return function(props)
     local variant = props.variant or "header"
     local justification = props.justification or "center_justify"
 
-    local widgetPath = widget.path .. "buttons/" .. name .. "table_button.ui_widget_definition"
+    local stringsTagPath
+    if text then
+        -- Generate strings tag
+        stringsTagPath = widget.path .. "strings/" .. name .. "_table_button.unicode_string_list"
+        ustr(stringsTagPath, {text})
+    end
+
+    local widgetPath = widget.path .. "buttons/" .. name .. "_table_button.ui_widget_definition"
     ---@type invaderWidget
     local wid = {
         widget_type = "text_box",
         bounds = widget.bounds(0, 0, 19, 104),
         background_bitmap = [[insurrection/ui/bitmaps/browser_upper_button.bitmap]],
-        flags = {pass_unhandled_events_to_focused_child = true}
+        flags = {pass_unhandled_events_to_focused_child = true},
+        text_label_unicode_strings_list = stringsTagPath,
+        string_list_index = 0,
+        text_font = constants.fonts.button,
+        text_color = constants.color.text,
+        justification = justification,
+        vert_offset = 2,
     }
+    if variant == "row" then
+        wid.background_bitmap = [[insurrection/ui/bitmaps/browser_table_button.bitmap]]
+        wid.bounds = widget.bounds(0, 0, 19, 528)
+    end
 
     widget.createV2(widgetPath, wid)
     return widgetPath
