1360cb8ebdf7c99b89f63f6f91d8597e19ac7b82
markmcfuzz <magecape10@gmail.com>
2024-07-02 14:41:33 -0600

    add credit variant, scale credit icon

--- lua/scripts/ui/componentsV3/rankDashboard.lua
+++ lua/scripts/ui/componentsV3/rankDashboard.lua
@@ -8,7 +8,7 @@ local floor = math.floor
 ---@field name string
 ---@field text? string
 ---@field justification? "left_justify" | "center_justify" | "right_justify"
----@field variant "overlay" | "icon" | "text_rank_name" | "text_spec_info" | "text_level_info" | "text_credits_info"
+---@field variant "overlay" | "icon" | "credit_icon" 
 ---@field childs? invaderWidgetChildWidget[]
 
 ---Generic rank dashboard for info
@@ -33,6 +33,12 @@ return function(props)
         scale = 0.24
     }
 
+    local creditIconSize = {
+        width = 100,
+        height = 100,
+        scale = 0.16
+    }
+
     ---@type invaderWidget
     local wid = {
         widget_type = "text_box",
@@ -50,28 +56,9 @@ return function(props)
     if variant == "icon" then
         wid.background_bitmap = [[insurrection/ui/bitmaps/rank_dummy.bitmap]]
         wid.bounds = widget.scale(rankIconSize.width, rankIconSize.height, rankIconSize.scale)
-    end
-
-    if variant == "text_rank_name" then
-        wid.background_bitmap = [[insurrection/ui/bitmaps/rank_text_box.bitmap]]
-        --wid.bounds = widget.scale(rankTextBoxSize.width, rankTextBoxSize.height, rankTextBoxSize.scale)
-        wid.text_color = constants.color.title
-        wid.bounds = widget.bounds(0, 0, 61, 128)
-    elseif variant == "text_spec_info" then
-        wid.background_bitmap = [[insurrection/ui/bitmaps/rank_text_box.bitmap]]
-        --wid.bounds = widget.scale(rankTextBoxSize.width, rankTextBoxSize.height, rankTextBoxSize.scale)
-        wid.text_color = constants.color.subtitle
-        wid.bounds = widget.bounds(0, 0, 61, 128)
-    elseif variant == "text_level_info" then
-        wid.background_bitmap = [[insurrection/ui/bitmaps/rank_text_box.bitmap]]
-        --wid.bounds = widget.scale(rankTextBoxSize.width, rankTextBoxSize.height, rankTextBoxSize.scale)
-        wid.text_color = constants.color.selected
-        wid.bounds = widget.bounds(0, 0, 61, 128)
-    elseif  variant == "text_credits_info" then
-        wid.background_bitmap = [[insurrection/ui/bitmaps/rank_text_box.bitmap]]
-        --wid.bounds = widget.scale(rankTextBoxSize.width, rankTextBoxSize.height, rankTextBoxSize.scale)
-        wid.text_color = constants.color.title
-        wid.bounds = widget.bounds(0, 0, 61, 128)
+    elseif variant == "credit_icon" then
+        wid.background_bitmap = [[insurrection/ui/bitmaps/credit_icon.bitmap]]
+        wid.bounds = widget.scale(creditIconSize.width, creditIconSize.height, creditIconSize.scale)
     end
 
     widget.createV2(widgetPath, wid)
