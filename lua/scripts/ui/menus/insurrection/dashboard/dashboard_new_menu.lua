f9b41cd9564ad77f0c0af698a0845b4edd09b8f2
markmcfuzz <magecape10@gmail.com>
2024-07-02 02:32:11 -0600

    add rank component

--- lua/scripts/ui/menus/insurrection/dashboard/dashboard_new_menu.lua
+++ lua/scripts/ui/menus/insurrection/dashboard/dashboard_new_menu.lua
@@ -5,6 +5,7 @@ local preview = require "lua.scripts.ui.componentsV3.preview"
 local bar = require "lua.scripts.ui.componentsV3.bar"
 local buttonDashboard = require "lua.scripts.ui.componentsV3.buttonDashboard"
 local footer          = require "lua.scripts.ui.componentsV3.footer"
+local rankDashboard   = require "lua.scripts.ui.componentsV3.rankDashboard"
 local pos = constants.position
 local label = require "lua.scripts.ui.componentsV3.label"
 local strmem = widget.strmem
@@ -12,8 +13,6 @@ local container = require "lua.scripts.ui.componentsV3.container"
 local options = require "lua.scripts.ui.componentsV3.options"
 local button = require "lua.scripts.ui.componentsV3.button"
 local header = require "lua.scripts.ui.componentsV3.header"
-local buttonTable = require "lua.scripts.ui.componentsV3.buttonTable"
-local complexButton = require "lua.scripts.ui.componentsV3.complexButton"
 
 widget.init [[insurrection/ui/menus/dashboard_new/]]
 
@@ -90,20 +89,17 @@ return container {
                             text = "Welcome to the dashboard!\r\nHere you can choose from different game modes from Insurrection services."
                         },
                         borrowLayout()
+                    },
+                    {
+                        rankDashboard {
+                            name = "rank_dashbard",
+                            bitmap = [[insurrection/ui/bitmaps/rank_overlay.bitmap]]
+                        },
+                        776,
+                        358
                     }
                 }
             },
-        },
-        {
-            options {
-                name = "dashboard_rank_info",
-                alignment = "horizontal",
-                childs {
-                    {
-                        
-                    }
-                }
-            }
         },
         {constants.components.currentProfile.path, 641, 20},
         {constants.components.version.path, 0, 460},
