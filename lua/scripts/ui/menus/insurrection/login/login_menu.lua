9bd0f19ecf9c18cf62fadb6b723a3a75f626d757
Ivanglpz <miltonivan117@gmail.com>
2025-01-03 21:05:25 -0600

    migrate: login components UI to v3

--- lua/scripts/ui/menus/insurrection/login/login_menu.lua
+++ lua/scripts/ui/menus/insurrection/login/login_menu.lua
@@ -1,11 +1,11 @@
 local widget = require "lua.scripts.widget"
-local container = require "lua.scripts.ui.componentsV2.container"
-local options = require "lua.scripts.ui.componentsV2.options"
-local button = require "lua.scripts.ui.componentsV2.button"
-local header = require "lua.scripts.ui.componentsV2.header"
-local input = require "lua.scripts.ui.componentsV2.input"
+local container = require "lua.scripts.ui.componentsV3.container"
+local options = require "lua.scripts.ui.componentsV3.options"
+local button = require "lua.scripts.ui.componentsV3.button"
+local header = require "lua.scripts.ui.componentsV3.header"
+local input = require "lua.scripts.ui.componentsV3.input"
 local constants = require "lua.scripts.ui.components.constants"
-local image = require "lua.scripts.ui.componentsV2.image"
+local image = require "lua.scripts.ui.componentsV3.image"
 local pos = constants.position
 
 widget.init [[insurrection/ui/menus/login/]]
@@ -13,41 +13,94 @@ widget.init [[insurrection/ui/menus/login/]]
 local layout = widget.align("vertical", 33, 328, 180, 14)
 local layout2 = widget.align("vertical", 24, 328, 310, 2)
 
-return container("login_menu", {
-    {
-        header("login", "INSURRECTION LOGIN",
-               "LOG INTO YOUR INSURRECTION ACCOUNT, GET ACCESS TO DIFFERENT SERVICES"),
+local containerLogin =  container {
+    name ="login_menu",
+    background ="transparent",
+    childs = {
+        {
+       header {
+             name = "login",
+             title = "INSURRECTION LOGIN",
+             subtitle = "LOG INTO YOUR INSURRECTION ACCOUNT, GET ACCESS TO DIFFERENT SERVICES"
+        },
         pos.header.x,
         pos.header.y
     },
     {
-        image("dialog_background", [[insurrection/ui/bitmaps/login_dialog_background.bitmap]], 334,
-              345),
+        image {
+            name = "dialog_background",
+            width= 334,
+            height = 345,
+            image = [[insurrection/ui/bitmaps/login_dialog_background.bitmap]]
+        },
+
         253,
         78
     },
     {
-        image("insurrection_logo", [[insurrection/ui/bitmaps/insurrection_logo.bitmap]], 1280, 225,
-              0.15),
+        image {
+            name = "insurrection_logo",
+            width = 1280,
+            height = 225,
+            image =  [[insurrection/ui/bitmaps/insurrection_logo.bitmap]],
+            scale  = 0.15
+        },
         322,
         113
     },
     {
-        options("login_options", "vertical", {
+     options {
+        name = "login_options",
+        layout ="vertical",
+        childs = {
             {
-                input("username", string.rep(" ", 23),
-                      {icon = "insurrection/ui/bitmaps/user_icon.bitmap"}),
-                layout()
+                input {
+                   name = "username",
+                  text = string.rep(" ", 23),
+                   icon =  "insurrection/ui/bitmaps/user_icon.bitmap",
+                 },
+                  layout()
             },
             {
-                input("password", string.rep(" ", 23),
-                      {icon = [[insurrection/ui/bitmaps/lock_icon.bitmap]], password = true}),
-                layout()
+                input {
+                    name ="password",
+                    text =  string.rep(" ", 23),
+                    icon = [[insurrection/ui/bitmaps/lock_icon.bitmap]],
+                    password = true
+                },
+                 layout()
             },
-            {button("login", "LOGIN", {justification = "center_justify"}), layout2(-12)},
-            {button("register", "REGISTER", {justification = "center_justify"}), layout2()},
-            {button("back", "BACK", {justification = "center_justify", back = true}), layout2()}
-        })
-    },
-    {constants.components.version.path, 0, 460}
-})
+            {
+                button {
+                    name ="login",
+                   text = "LOGIN",
+                   justification ="center_justify",
+                },
+                layout2(-12)
+            },
+             {
+                button {
+                    name = "register",
+                    text ="REGISTER",
+                    justification = "center_justify",
+
+                },
+                 layout2()
+             },
+             {
+                button {
+                    name ="back",
+                    text ="BACK",
+                    justification = "center_justify",
+                    back = true
+                },
+                layout2()
+             }
+         }
+        }
+     },
+      {constants.components.version.path, 0, 460}
+    }
+}
+
+return containerLogin
