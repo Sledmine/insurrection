e98f1207e14adb5af48b9d7bd67197c38cbdece0
Ivan Garcia <67771688+ivanglpz@users.noreply.github.com>
2025-01-24 21:18:31 -0600

    ui menus insurrection pause migrate components to v3
    * migration: pause game options menu components to v3
    
    * migration: pause menu components to v3

--- lua/scripts/ui/menus/insurrection/pause/pause_game_options_menu.lua
+++ lua/scripts/ui/menus/insurrection/pause/pause_game_options_menu.lua
@@ -1,8 +1,10 @@
 local widget = require "lua.scripts.widget"
-local container = require "lua.scripts.ui.componentsV2.container"
-local header = require "lua.scripts.ui.componentsV2.header"
-local options = require "lua.scripts.ui.componentsV2.options"
-local button = require "lua.scripts.ui.componentsV2.button"
+local container = require "lua.scripts.ui.componentsV3.container"
+local header = require "lua.scripts.ui.componentsV3.header"
+local options = require "lua.scripts.ui.componentsV3.options"
+
+local button = require "lua.scripts.ui.componentsV3.button"
+
 local constants = require "lua.scripts.ui.components.constants"
 local pos = constants.position
 
@@ -10,26 +12,51 @@ widget.init [[insurrection/ui/menus/pause/]]
 
 local layout = widget.align("vertical", 24, pos.options.x, pos.options.y, 2)
 
-return container("pause_game_options_menu", {
-    {
-        header("pause_game_options_menu", "GAME OPTIONS", "SELECT AND CUSTOMIZE SOME GAME OPTIONS"),
-        pos.header.x,
-        pos.header.y
-    },
-    {
-        options("pause_game_options", "vertical", {
-            {
-                button("choose_team", "CHOOSE TEAM", {
-                    opens = [[insurrection/ui/menus/pause/pause_choose_team_menu.ui_widget_definition]]
-                }),
-                layout()
+local containerPauseGameOptionsMenu container {
+    name = "pause_game_options_menu",
+    background = "transparent",
+    childs = {
+        {
+            header {
+                name = "pause_game_options_menu",
+                title = "GAME OPTIONS",
+                subtitle = "SELECT AND CUSTOMIZE SOME GAME OPTIONS"
             },
-            {[[insurrection/ui/shared/void.ui_widget_definition]]},
-            {[[insurrection/ui/shared/void.ui_widget_definition]]},
-            {button("back", "BACK", {back = true}), layout()}
-        }, nil, {func = "mp_game_options_open"})
-    },
-    {constants.components.currentProfile.path, 624, 20},
-    {constants.components.version.path, 0, 460}
-})
+            pos.header.x,
+            pos.header.y
+        },
+        {
+            options {
+                name ="pause_game_options",
+                layout = "vertical",
+                description = nil,
+                func = "mp_game_options_open",
+                childs = {
+                    {
+                        button {
+                             name ="choose_team",
+                             text ="CHOOSE TEAM",
+                             opens =  [[insurrection/ui/menus/pause/pause_choose_team_menu.ui_widget_definition]]
+                         },
+                         layout()
+                    },
+                    {[[insurrection/ui/shared/void.ui_widget_definition]]},
+                    {[[insurrection/ui/shared/void.ui_widget_definition]]},
+                    {
+                        button {
+                            name ="back",
+                            text ="BACK",
+                            back= true
+                        },
+                        layout()
+                    }
+                }
+            }
+        },
+        {constants.components.currentProfile.path, 624, 20},
+        {constants.components.version.path, 0, 460}
+    }
+}
+
+return containerPauseGameOptionsMenu
 
--- lua/scripts/ui/menus/insurrection/pause/pause_menu.lua
+++ lua/scripts/ui/menus/insurrection/pause/pause_menu.lua
@@ -1,8 +1,8 @@
 local widget = require "lua.scripts.widget"
-local container = require "lua.scripts.ui.componentsV2.container"
-local header = require "lua.scripts.ui.componentsV2.header"
-local options = require "lua.scripts.ui.componentsV2.options"
-local button = require "lua.scripts.ui.componentsV2.button"
+local container = require "lua.scripts.ui.componentsV3.container"
+local header = require "lua.scripts.ui.componentsV3.header"
+local options = require "lua.scripts.ui.componentsV3.options"
+local button = require "lua.scripts.ui.componentsV3.button"
 local constants = require "lua.scripts.ui.components.constants"
 local pos = constants.position
 
@@ -10,38 +10,81 @@ widget.init [[insurrection\ui\menus\pause\]]
 
 local layout = widget.align("vertical", 24, pos.options.x, pos.options.y, 2)
 
-return container("pause_menu", {
-    {
-        header("pause_menu", "PAUSE MENU", "GET BACK IN THE FIELD SPARTAN!"),
-        pos.header.x,
-        pos.header.y
-    },
-    {
-        options("pause_menu_options", "vertical", {
-            {button("resume_game", "RESUME GAME", {close = true}), layout()},
-            {
-                button("game_options", "GAME OPTIONS", {
-                    opens = [[insurrection/ui/menus/pause/pause_game_options_menu.ui_widget_definition]]
-                }),
-                layout()
+local containerPauseMenu  container {
+    name = "pause_menu",
+    background = "transparent",
+    childs = {
+        {
+            header {
+                name = "pause_menu",
+                title = "PAUSE MENU",
+                subtitle = "GET BACK IN THE FIELD SPARTAN!"
             },
-            {
-                button("settings", "SETTINGS", {
-                    opens = [[insurrection/ui/menus/settings/settings_menu.ui_widget_definition]],
-                    func = "profile_set_edit_begin",
-                    branch = true
-                }),
-                layout()
-            },
-            {
-                button("mods", "MODS",
-                       {opens = [[insurrection/ui/menus/mods/mods_menu.ui_widget_definition]]}),
-                layout()
-            },
-            {button("open_map_pause", "MAP MENU", {}), layout()},
-            {button("exit", "EXIT TO MAIN MENU", {func = "mp_game_player_quit"}), layout()}
-        }, nil, {func = "mp_pause_menu_open"})
-    },
-    {constants.components.currentProfile.path, 624, 20},
-    {constants.components.version.path, 0, 460}
-})
+            pos.header.x,
+            pos.header.y
+        },
+        {
+            options {
+                name = "pause_menu_options",
+                layout = "vertical",
+                func = "mp_pause_menu_open",
+                description = nil,
+                childs ={
+                    {
+                        button {
+                            name = "resume_game",
+                            text = "RESUME GAME",
+                            close = true
+                        },
+                        layout()
+                    },
+                    {
+                        button {
+                            name = "game_options",
+                            text = "GAME OPTIONS",
+                            opens =  [[insurrection/ui/menus/pause/pause_game_options_menu.ui_widget_definition]]
+                        },
+                        layout()
+                    },
+                    {
+                        button {
+                            name = "settings",
+                            text = "SETTINGS",
+                            opens = [[insurrection/ui/menus/settings/settings_menu.ui_widget_definition]],
+                            func = "profile_set_edit_begin",
+                            branch = true
+                        },
+                        layout()
+                    },
+                    {
+                        button {
+                            name = "mods",
+                            text = "MODS",
+                            opens = [[insurrection/ui/menus/mods/mods_menu.ui_widget_definition]]
+                        },
+                        layout()
+                    },
+                    {
+                        button {
+                            name = "open_map_pause",
+                            text = "MAP MENU"
+                        },
+                        layout()
+                    },
+                    {
+                        button {
+                            name ="exit",
+                            text = "EXIT TO MAIN MENU",
+                            func = "mp_game_player_quit"
+                        },
+                        layout()
+                    }
+                }
+            }
+        },
+        {constants.components.currentProfile.path, 624, 20},
+        {constants.components.version.path, 0, 460}
+    }
+}
+
+return containerPauseMenu
