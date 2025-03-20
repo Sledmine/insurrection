58cbead8b60a9cd5ea9ebd1f0ec0e083fd98f8b7
markmcfuzz <magecape10@gmail.com>
2025-03-20 00:41:37 -0600

    add labels on row button

--- lua/scripts/ui/menus/insurrection/lobby/lobby_browser_table_menu.lua
+++ lua/scripts/ui/menus/insurrection/lobby/lobby_browser_table_menu.lua
@@ -33,12 +33,66 @@ local optionsLayout = widget.layout {
 
 local rows = {}
 for i = 1, 8 do
+    local tableRowLabelLayout = widget.layout{
+        alignment ="horizontal",
+        size = 104,
+        horizontal = 24,
+        margin = 0,
+        x = 4,
+        y = -3
+    }
     table.insert(rows, {
         buttonTable {
             name = "list_browser_row_button_" .. i,
             variant = "row",
             --text = strmem(256, "Lobby " .. i),
-            childs = {{label {name = "owner_header_label", text = "OWNER"}}}
+            childs = {
+                {
+                    label {
+                        name = "owner_header_label",
+                        text = strmem(11, "OWNER"),
+                        justify = "center",
+                        width = 104,
+                    },
+                    tableRowLabelLayout()
+                },
+                    {
+                        label{
+                            name = "map_header_label",
+                            text = strmem(64, "MAP"),
+                            justify = "center",
+                            width = 104,
+                            },
+                            tableRowLabelLayout()
+                    },
+                    {
+                        label{
+                            name = "gametype_header_label",
+                            text = strmem(32, "GAMETYPE"),
+                            justify = "center",
+                            width = 104,
+                            },
+                            tableRowLabelLayout()
+                    },
+                    {
+                        label{
+                            name = "players_header_label",
+                            text = "000",
+                            justify = "center",
+                            width = 104,
+                            },
+                            tableRowLabelLayout()
+                    },
+                    {
+                        label{
+                            name = "ping_header_label",
+                            text = "000",
+                            justify = "center",
+                            width = 104,
+                            },
+                            tableRowLabelLayout()
+                    },
+                },
         },
         tableRowLayout()
     })
