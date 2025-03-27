09d2ee0400056a31418bb0e288b22d63c4ba1d17
Ivanglpz <miltonivan117@gmail.com>
2025-03-26 23:45:14 -0600

    add map preview and map set name script

--- lua/modules/insurrection/components/dynamic/lobbyBrowserMenu.lua
+++ lua/modules/insurrection/components/dynamic/lobbyBrowserMenu.lua
@@ -1,28 +1,51 @@
 local components = require "insurrection.components"
 local constants = require "insurrection.constants"
-local button = require "insurrection.components.button"
-local input = require "insurrection.components.input"
 local list = require "insurrection.components.list"
 local bar = require "insurrection.components.bar"
-local interface = require "insurrection.interface"
 local getState = require "insurrection.redux.getState"
 local blam = require "blam"
-local utils = require "insurrection.utils"
 local t = require"insurrection.utils".snakeCaseToTitleCase
+local core = require "insurrection.core"
 
 return function()
     local state = getState()
     local browser = components.new(constants.widgets.browser.id)
-    local preview = components.new(browser:findChildWidgetTag("preview").id)
     local lobbies = list.new(browser:findChildWidgetTag("table_row_list").id)
     local scrollBar = bar.new(browser:findChildWidgetTag("table_scroll").id, "scroll")
+    local mapPreview = components.new(browser:findChildWidgetTag("table_preview").id)
+    local mapName = components.new(browser:findChildWidgetTag("table_map_name").id)
 
+    local function getMapBackgroundBitmap(mapName)
+        local mapCollection = blam.tagCollection(constants.tagCollections.maps.id)
+        assert(mapCollection, "No map preview collection found")
+        for k, v in pairs(mapCollection.tagList) do
+            local bitmapTag = blam.getTag(v) --[[@as tag]]
+            -- bitmapTag.
+            local mapBitmaName = core.getTagName(bitmapTag.path):lower()
+
+            if mapBitmaName == mapName:lower() then
+                return bitmapTag.id
+            end
+        end
+        return constants.bitmaps.unknownMapPreview.id
+    end
+
+        local function setMapBackgroundBitmap(mapName)
+        mapPreview.widgetDefinition.backgroundBitmap = getMapBackgroundBitmap(mapName)
+    end
+
+    lobbies:onSelect(function (item)
+        local lobby = state.lobbies[item.value]
+        setMapBackgroundBitmap(lobby.map)
+        mapName:setText(t(lobby.map))
+    end)
     lobbies:setScrollBar(scrollBar)
     lobbies:scrollable(false)
 
     local loadLobbies = function()
-        lobbies:setItems(table.map((state.lobbies or {}), function(lobby)
+        lobbies:setItems(table.map((state.lobbies or {}), function(lobby,lobbyIndex)
             return {
+                value = lobbyIndex,
                 ---@param item uiComponent
                 label = function(item)
                     local owner = components.new(item:findChildWidgetTag("owner_header_label").id)
