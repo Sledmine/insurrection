bf68919300c58ff9356eafd0a512423e6bf51630
markmcfuzz <magecape10@gmail.com>
2025-04-02 03:59:07 -0600

    added objects counter label, adjusted list space offset

--- lua/scripts/ui/menus/insurrection/customization/customization_biped.lua
+++ lua/scripts/ui/menus/insurrection/customization/customization_biped.lua
@@ -13,10 +13,11 @@ local slider = require "lua.scripts.ui.componentsV3.slider"
 
 widget.init [[insurrection/ui/menus/customization_biped/]]
 
-local yPosList = 305
+
+local yPosList = 290
 local barSize = 200
-local layout = widget.align("horizontal", constants.components.complexButton.normal.width, 59,
-                            yPosList, 2)
+local layout = widget.align("horizontal", constants.components.complexButton.normal.width, 57,
+                            yPosList, 4)
 
 return container {
     name = "customization_biped_menu",
@@ -32,8 +33,24 @@ return container {
             pos.header.y
         },
         {
-            label {name = "geometry_name", text = strmem(32, "GEOMETRY_NAME"), variant = "title"},
-            53,
+            label {
+                name = "geometry_name",
+                text = strmem(32, "GEOMETRY_NAME"),
+                variant = "title"
+            },
+            56,
+            yPosList - 26
+        },
+        {
+            label {
+                name = "geometry_item_counter",
+                text = strmem(8, "00/00"),
+                justify = "right",
+                variant = "title",
+                color = "blueYonder",
+                width = 854 - 57
+            },
+            0,
             yPosList - 26
         },
         {
@@ -44,14 +61,21 @@ return container {
                 size = barSize
             },
             (constants.screen.width / 2) - (barSize / 2),
-            425
+            410
         },
         {
             options {
                 name = "customization_geometry_list",
                 alignment = "horizontal",
                 childs = {
-                    {slider {name = "geometry_left", direction = "left"}, pos.options.x, yPosList},
+                    {
+                        slider {
+                            name = "geometry_left",
+                            direction = "left"
+                        },
+                        38,
+                        yPosList
+                    },
                     {
                         complexButton {
                             name = "geometry_item_1",
@@ -100,16 +124,28 @@ return container {
                         },
                         layout()
                     },
-                    {slider {name = "geometry_right", direction = "right"}, layout()},
+                    {
+                        slider {
+                            name = "geometry_right",
+                            direction = "right",
+                            
+                        },
+                        layout()
+                    },
                     {
                         options {
                             name = "customization_geometry_actions",
                             layout = "horizontal",
                             childs = {
                                 {
-                                    button {name = "back", text = "BACK", back = true},
-                                    pos.back.x,
-                                    pos.back.y
+                                    button {
+                                        name = "back",
+                                        text = "BACK",
+                                        variant = "small",
+                                        back = true
+                                    },
+                                    pos.back_left.x,
+                                    pos.back_left.y
                                 }
                             }
                         }
@@ -117,7 +153,7 @@ return container {
                 }
             }
         },
-        {constants.components.currentProfile.path, 624, 20},
-        {constants.components.version.path, 0, 460}
+        {constants.components.currentProfile.path, pos.nameplate.x, pos.nameplate.y},
+        {constants.components.version.path, pos.version.x, pos.version.y}
     }
 }
