0ff4b56b43b7e524d3efbf365735a1b5e7a4a6cd
markmcfuzz <magecape10@gmail.com>
2025-08-22 16:05:38 -0600

    updated skulls menu

--- lua/scripts/ui/menus/insurrection/lobby/lobby_skulls_menu.lua
+++ lua/scripts/ui/menus/insurrection/lobby/lobby_skulls_menu.lua
@@ -24,6 +24,9 @@ widget.init [[insurrection/ui/menus/lobby_skulls/]]
 
 local defsLayout = widget.layout {alignment = "horizontal", size = 149, x = 20, y = 60, margin = 2}
 
+local wrapperWidth = 570
+local wrapperHeight = 247
+
 local buttonSquareMargin = 4
 local buttonSquareSize = 80 - buttonSquareMargin
 local rowsCount = 3
@@ -135,11 +138,105 @@ return container {
                 }
             }
         },
+        {options {
+            name = "skull_grid",
+            alignment = "horizontal",
+            childs = skullElements
+        }
+    },
+        {
+            bar {
+                name = "skull_bar",
+                orientation = "vertical",
+                type = "scroll",
+                size = 222
+            },
+            410,
+            141
+        },
+        {
+            image{
+                name = "skull_preview_background",
+                bitmap = "insurrection/ui/bitmaps/skull_overlay.bitmap",
+                width = 149,
+                height = 144
+            },
+            426,
+            141
+        },
+        {
+            image{
+                name = "skull_preview_icon",
+                bitmap = "insurrection/ui/bitmaps/skull_preview_icons.bitmap",
+                width = 136,
+                height = 178,
+                scale = 0.45
+            },
+            469,
+            151
+        },
+        {
+            label {
+                name = "skull_title",
+                variant = "title",
+                text = "CHOOSE SKULLS",
+                justify = "left",
+                width = 145
+            },
+            20,
+            116
+        },
+        {
+            label {
+                name = "skull_name",
+                variant = "title",
+                text = strmem(128, "SKULL NAME"),
+                justify = "left",
+                width = 145
+            },
+            430,
+            262
+        },
+        {
+            label {
+                name = "skull_motto",
+                variant = "button",
+                text = strmem(256, "SKULL MOTTO"),
+                color = "blueYonder",
+                justify = "left",
+                width = 145
+            },
+            430,
+            283
+        },
+        {
+            label {
+                name = "skull_description",
+                variant = "subtitle",
+                text = strmem(256, "SKULL DESCRIPTION"),
+                color = "gray",
+                justify = "left",
+                width = 145,
+                height = 35
+            },
+            430,
+            304
+        },
         {
             options {
-                name = "skull_grid",
+                name = "skull_options",
                 alignment = "horizontal",
-                childs = skullElements
+                childs = {
+                    {
+                        button {
+                            name = "skull_settings",
+                            text = "SKULL SETTINGS",
+                            variant = "small",
+                        },
+                        426,
+                        339
+                    }
+                }
             }
         },
         {constants.components.version.path, 0, 460}
