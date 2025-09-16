a8ba0617890cd395425ae40e117f89fe4fd17866
Sledmine <40512223+Sledmine@users.noreply.github.com>
2025-09-16 02:11:48 -0600

    Reorganize metadata for custom projects

---
+++ lua/modules/insurrection/constants/projects/firefight.lua
@@ -0,0 +1,33 @@
+return {
+    name = "Firefight Community Maps",
+    description = "A collection of community-made maps\nfor Firefight mode.",
+    author = "Insurrection",
+    maps = {
+        firefight = {
+            {
+                name = "firefight-descent",
+                title = "Descent",
+                description = "A wise man once said: one does not simply\n walk into Descent.\n(Single Player)",
+                author = "MoooseGuy & Pepzee"
+            },
+            {
+                name = "firefight-portent",
+                title = "Portent",
+                description = "A small Forerunner installation on a\nremote planet.\n(Single Player)",
+                author = "Gamma927 & Others"
+            },
+            {
+                name = "firefight-sector-09",
+                title = "Sector 09",
+                description = "A dark city sector overrun by the Covenant.\nHold the line through relentless waves.\n(Single Player)",
+                author = "Nickster5000 / Insurrection Team"
+            },
+            {
+                name = "alpha-halo",
+                title = "Alpha Halo",
+                description = "The first Halo ring discovered by humanity.\nFight for survival on sacred ground.\n(Single Player)",
+                author = "Insurrection Team"
+            }
+        }
+    }
+}
---
+++ lua/modules/insurrection/constants/projects/helljumper.lua
@@ -0,0 +1,39 @@
+return {
+    name = "Helljumper",
+    description = "A series of multiplayer maps inspired by\nthe Helljumper novel by Eric Nylund.",
+    author = "Keymind Studios",
+    maps = {
+        multiplayer = {
+            {
+                name = "aqueduct",
+                title = "Aqueduct",
+                description = "The resting place of countless warriors\nfrom an ever fleeting distant past.\n(2-8 players)",
+                author = "Keymind Studios"
+            },
+            {
+                name = "bleed_it_out",
+                title = "Bleed It Out",
+                description = "Yeah, here we go for a hundred time...\n(4-16 players, supports vehicles)",
+                author = "Keymind Studios"
+            },
+            {
+                name = "impasse",
+                title = "Impasse",
+                description = "Once, nearby telescopes listened for a\nmessage from the stars. Now these silos\ncontain our prepared response.\n(4-16 players, supports vehicles)",
+                author = "Keymind Studios"
+            },
+            {
+                name = "last_voyage",
+                title = "Last Voyage",
+                description = "Though you leave this world, you will come\non the Great Journey. Or not?\n(2-8 players)",
+                author = "Keymind Studios"
+            },
+            {
+                name = "treason",
+                title = "Treason",
+                description = "The Office of Naval Inteligence leaves no\nloose knots behind...\n(4-16 players)",
+                author = "Keymind Studios"
+            }
+        }
+    }
+}
