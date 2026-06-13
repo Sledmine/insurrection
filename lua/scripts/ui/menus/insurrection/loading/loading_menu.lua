7c39d963cf5319f6bc960b465949063a3fac0986
Mark <magecape10@gmail.com>
2026-06-12 20:26:05 -0700

    Add loading menu implementation with loading label and orb animation

---
+++ lua/scripts/ui/menus/insurrection/loading/loading_menu.lua
@@ -0,0 +1,44 @@
+local widget = require "lua.scripts.widget"
+local strmem = widget.strmem
+local constants = require "lua.scripts.ui.components.constants"
+local container = require "lua.scripts.ui.componentsV3.container"
+local label = require "lua.scripts.ui.componentsV3.label"
+local image = require "lua.scripts.ui.componentsV3.image"
+
+widget.init [[insurrection/ui/menus/loading/]]
+
+local name = "loading_menu"
+
+local loadingMenuPath = container {
+    name = name,
+    background = "transparent",
+    childs = {
+        {
+            label {
+                name = "loading_label",
+                text = strmem(256, "LOADING GAME..."),
+                justify = "center",
+                variant = "title",
+                childs = {
+                    {
+                        image {
+                            name = "loading_orb",
+                            bitmap = "insurrection/ui/bitmaps/loading_orb.bitmap",
+                            width = 256,
+                            height = 256,
+                            scale = 0.5
+                        },
+                        360,
+                        10
+                    }
+                }
+            },
+            0,
+            200
+        },
+    }
+}
+
+widget.global(loadingMenuPath, "insurrection/ui/custom_menus.tag_collection")
+
+return loadingMenuPath
