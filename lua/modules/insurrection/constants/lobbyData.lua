01aeaecdabbac54382058a47edd7ed473309a4f9
Mark <magecape10@gmail.com>
2026-06-21 21:34:39 -0700

    add new gametypes and templates descriptions to lobbyData

---
+++ lua/modules/insurrection/constants/lobbyData.lua
@@ -0,0 +1,57 @@
+return {
+    gametypes = {
+        unknown = {
+            description = "No information available for this element."
+        },
+        assault = {
+            description = "Teams alternate offense and defense every 2 minutes. Carry the enemy flag into their base to their flag station.\n5 assaults wins."
+        },
+        crazy_king = {
+            description = "Control the hill for a total of 2 minutes to win the match but watch out: it moves from place to place."
+        },
+        ctf = {
+            description = "The first team to capture the enemy flag and return it to their own base 3 times wins the game.\nYou must have your own flag to capture."
+        },
+        forge = {
+            description = "A free-for-all gametype where players can use the forge\nhammer to destroy or create objects in the map. The player\nwith the most kills at the end of 10 minutes wins."
+        },
+        infection = {
+            description = "One player starts as the infected and must infect other\nplayers by killing them. The last survivor wins or if time\nruns out, the most recent survivor wins."
+        },
+        juggernaut = {
+            description = "The Juggernaut does extra damage. Kill the Juggernaut to steal his powers. Score 15 kills to win the game."
+        },
+        king = {
+            description = "Control the hill for a total of 2 minutes to win the match."
+        },
+        oddball = {
+            description = "Hold the Skull for 2 minutes to win."
+        },
+        race = {
+            description = "The first player to complete 3 laps by touching all flag points wins the race."
+        },
+        slayer = {
+            description = "Classic deathmatch. 25 kills to win. Free for all.",
+        },
+        team_slayer = {
+            description = "The first team to amass 50 kills wins this fragfest. Players earn points by killing members of the opposite team."
+        },
+        team_oddball = {
+            description = "The first team to hold the Skull for 2 minutes wins. 5 second respawn time."
+        },
+        team_king = {
+            description = "The first team to control the hill for a total of 2 minutes wins. 10 second respawn time."
+        },
+        team_race = {
+            description = "The first team to complete 3 laps by touching all flag points in order 3 times wins the race."
+        }
+    },
+    templates = {
+        stock = {
+            description = "Play like the original map was intended to be played."
+        },
+        fiesta = {
+            description = "Random weapons every time you respawn."
+        }
+    }
+}
